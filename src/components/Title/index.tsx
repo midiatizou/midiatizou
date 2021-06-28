48d55c45fe9c1c5f8f2259ae5f368a68517b5b94
henrique <henrique.leao@softvaro.com.br>
2021-06-28 08:31:30 -0300

    feat: change all configuration

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -5,6 +5,6 @@ type TitleProps = {
 
 export const Title = ({ title }: TitleProps) => {
   return (
-    <h1 style={{ fontSize: '48px' }} dangerouslySetInnerHTML={{__html: title}} />
+    <h1 style={{ fontSize: '48px', color: '#D5D5D5' }} dangerouslySetInnerHTML={{__html: title}} />
   );
 }
